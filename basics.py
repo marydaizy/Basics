3b45ca47edd6ecdc769652f1478c5235772d2fa6
daisy <daisynakitende96@gmail.com>
2019-03-20 19:21:37 +0300

    Problem solving

---
+++ basics.py
@@ -0,0 +1,7 @@
+s = 'You may need to turn the string into an array before you can reverse it.'[::-1]
+print (s)
+
+word_list = ['Your','response', 'should', 'be', 'a', 'number']
+longest_word = max(word_list, key=len)
+print len(longest_word)
+
